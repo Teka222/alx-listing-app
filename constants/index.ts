4ca8048f7edeb748ddd5702eb7a3dc47d09fed56
Tekano Meso <tekanonetflorist01@gmail.com>
2025-08-17 15:51:45 +0200

    Create index.ts

---
+++ constants/index.ts
@@ -0,0 +1,13 @@
+// constants/index.ts
+
+export const PLACEHOLDER_IMAGE = "/assets/placeholder.jpg";
+export const APP_NAME = "ALX Listing App";
+
+export const SAMPLE_PROPERTY = {
+  name: "Cozy Apartment",
+  city: "Berlin",
+  country: "Germany",
+  price: 1200,
+  rating: 4.5,
+  image: PLACEHOLDER_IMAGE,
+};
